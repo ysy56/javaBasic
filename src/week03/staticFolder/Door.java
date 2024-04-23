e661fa38d18e6fda6ec9001fae2e8000df48ca78
moony <78634780+ysy56@users.noreply.github.com>
2024-04-23 17:53:56 +0900

    [3-6] 인스턴스 멤버와 클래스 멤버

---
+++ src/week03/staticFolder/Door.java
@@ -0,0 +1,7 @@
+package week03.staticFolder;
+
+public class Door {
+    public Door() {
+        System.out.println("Door");
+    }
+}
---
+++ src/week03/staticFolder/Handle.java
@@ -0,0 +1,7 @@
+package week03.staticFolder;
+
+public class Handle {
+    public Handle() {
+        System.out.println("Handle");
+    }
+}
