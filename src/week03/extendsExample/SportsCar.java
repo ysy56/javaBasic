a38c2b083caa0017e3ac5972cb768c011590c82c
moony <78634780+ysy56@users.noreply.github.com>
2024-04-24 10:14:49 +0900

    [3-11] 상속 개념 소개 및 예시

---
+++ src/week03/extendsExample/SportsCar.java
@@ -0,0 +1,8 @@
+package week03.extendsExample;
+
+public class SportsCar extends Car{
+    String engine;
+    public void booster() {
+        System.out.println("엔진 " + engine + " 부앙~\n");
+    }
+}
