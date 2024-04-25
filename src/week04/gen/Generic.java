a60f39108fc285c7b44e720634c136c817813fd0
moony <78634780+ysy56@users.noreply.github.com>
2024-04-25 15:49:41 +0900

    [4-6] 제네릭 개념 및 기본문법 소개

---
+++ src/week04/gen/Generic.java
@@ -0,0 +1,26 @@
+package week04.gen;
+
+// 1.
+public class Generic<T> {
+    // 2.
+    private T t;
+    // 3.
+    public T get() {
+        return this.t;
+    }
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public static void main(String[] args) {
+        // 4.
+        Generic<String> stringGeneric = new Generic<>();
+        // 5.
+        stringGeneric.set("Hello World");
+
+        String tValueTurnOutWithString = stringGeneric.get();
+
+        System.out.println(tValueTurnOutWithString);
+    }
+}
